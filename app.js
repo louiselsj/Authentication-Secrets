a1c4e755b755e17055cb3673a580851fb2dbf215
louis <louise.lsj@gmail.com>
2021-04-04 11:15:24 +0200

    Bcrypt

--- app.js
+++ app.js
@@ -5,11 +5,12 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+
 
 const app = express();
 
-console.log(md5("987654"));
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
@@ -21,7 +22,7 @@ mongoose.connect("mongodb://localhost:27017/userDB", {
   useNewUrlParser: true
 });
 
-const userSchema = new mongoose.Schema ({
+const userSchema = new mongoose.Schema({
   email: String,
   password: String
 });
@@ -44,23 +45,29 @@ app.get("/register", function(req, res) {
 
 
 app.post("/register", function(req, res) {
-  const newUser = new User({
-    email: req.body.username,
-    password: md5(req.body.password)
+
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+
+    const newUser = new User({
+      email: req.body.username,
+      password: hash
+    });
+
+    newUser.save(function(err) {
+      if (err) {
+        console.log(err);
+      } else {
+        res.render("secrets");
+      }
+    });
   });
 
-  newUser.save(function(err) {
-    if (err) {
-      console.log(err);
-    } else {
-      res.render("secrets");
-    }
-  });
+
 });
 
 app.post("/login", function(req, res) {
   const username = req.body.username;
-  const password = md5(req.body.password);
+  const password = req.body.password;
 
   User.findOne({
     email: username
@@ -69,9 +76,14 @@ app.post("/login", function(req, res) {
       console.log(err);
     } else {
       if (foundUser) {
-        if (foundUser.password === password) {
-          res.render("secrets");
-        }
+        bcrypt.compare(password, foundUser.password, function(err, result) {
+          if (result === true) {
+            res.render("secrets");
+          }
+
+        });
+
+
       }
     }
   });
